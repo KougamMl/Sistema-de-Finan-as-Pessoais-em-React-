e3dba7b039fd841584230dc5437ce85afd071598
kougam <kougam_lemke@hotmail.com>
2021-10-15 19:52:16 -0300

    second commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.18 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 27 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2400 },
+    { date: new Date(2021, 9, 18), category: 'sarary', title: 'Salário Google', value: 8600 },
+]; 
